f69f6e741601b28099ad8a1fce0ecf6275690cdb
Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2017-10-25 15:34:45 -0400

    added js to move navbar while scrolling

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,23 @@
+//javascript to have smooth transition on links and buttons for scrolling
 function scrollButtons(buttonClicked) {
 let scrollToSection = buttonClicked;
 document.querySelector(scrollToSection).scrollIntoView({ behavior: 'smooth'});
 }
+
+/**I had to use jquery to transition navigation bar from static to fixed and vice versa.
+bootstrap 4 dropped .affix sadly. I could have used postion: sticky polyfills as well
+  **/
+$(document).ready(function() {
+          $(window).on('scroll', function (event) {
+            $(".navbar").hide();
+
+              var scrollValue = $(window).scrollTop();
+              if (scrollValue > 600) {
+
+                  $('.navbar').addClass('fixed-top').show(); // try have fadIn() to work to smooth it out
+              } else{
+                  $('.navbar').removeClass('fixed-top');
+
+              }
+                });
+                  });
